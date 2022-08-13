cf1081adee75629f686f89871698971c916bed14
Fayzulla <fayzullarakhmatullayev@gmail.com>
2022-08-14 01:00:24 +0500

    created chatinput and chatmessages components and added functionality of Emojis

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import styled from "styled-components";
+import ChatInput from "./ChatInput";
 import Logout from "./Logout";
+import Messages from "./Messages";
 
 const ChatContainer = ({ currentChat }) => {
+  const handleSendMessage = async (msg) => {
+    console.log(msg);
+  };
   return (
     <>
       {currentChat && (
@@ -21,8 +26,8 @@ const ChatContainer = ({ currentChat }) => {
             </div>
             <Logout />
           </div>
-          <div className="chat-messages"></div>
-          <div className="chat-input"></div>
+          <Messages />
+          <ChatInput handleSendMessage={handleSendMessage} />
         </Container>
       )}
     </>
@@ -55,48 +60,6 @@ const Container = styled.div`
         h3 {
           color: white;
         }
-      }
-    }
-  }
-  .chat-messages {
-    padding: 1rem 2rem;
-    display: flex;
-    flex-direction: column;
-    gap: 1rem;
-    overflow: auto;
-    &::-webkit-scrollbar {
-      width: 0.2rem;
-      &-thumb {
-        background-color: #ffffff39;
-        width: 0.1rem;
-        border-radius: 1rem;
-      }
-    }
-    .message {
-      display: flex;
-      align-items: center;
-      .content {
-        max-width: 40%;
-        overflow-wrap: break-word;
-        padding: 1rem;
-        font-size: 1.1rem;
-        border-radius: 1rem;
-        color: #d1d1d1;
-        @media screen and (min-width: 720px) and (max-width: 1080px) {
-          max-width: 70%;
-        }
-      }
-    }
-    .sended {
-      justify-content: flex-end;
-      .content {
-        background-color: #4f04ff21;
-      }
-    }
-    .recieved {
-      justify-content: flex-start;
-      .content {
-        background-color: #9900ff20;
       }
     }
   }
